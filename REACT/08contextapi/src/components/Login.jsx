64e9b45e628ebcbb65b4a3bd168fb72c1b9ed581
Touseef Arshad <tauseefarshad23@icloud.com>
2024-01-09 00:05:51 +0500

    removing comments

--- REACT/08contextapi/src/components/Login.jsx
+++ REACT/08contextapi/src/components/Login.jsx
@@ -1,33 +1,4 @@
-// import React, { useContext } from 'react';
-// import UserContext from '../context/UserContext';
 
-// function Login() {
-
-//    const [Username,setUsername]= React.useState('')
-//    const [Password,setPassword]= React.useState('')
-//     const {setUser}= useContext(UserContext)
-//    const handleSumit=(e)=>{
-//     e.preventDefaut()
-//     setUser({Username, Password})
-//    }
-
-//     return (
-//         <div>
-//             <h2>Login</h2>
-//             <input type="text"
-//              value={Username}
-//              onChange={(e)=>setUsername(e.target.value)}
-//              placeholder='username' />
-//             <input type="text" 
-//             value={Password} 
-//             onChange={(e)=>setPassword(e.target.value)}
-//             placeholder='password' />
-//             <button onClick={handleSumit}>Submit</button>
-//         </div>
-//     );
-// }
-
-// export default Login;
 import React, {useState, useContext} from 'react'
 import UserContext from '../context/UserContext'
 
